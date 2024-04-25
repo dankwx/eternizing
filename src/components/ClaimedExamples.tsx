30800d5f36efdcd50b5d02afb076eeda8cb19021
Daniel Kondlatsch <danielkondlatsch.p@gmail.com>
2024-04-24 21:08:57 -0300

    feature: add youtube api integration to fetch video title and video thumbnail url

--- src/components/ClaimedExamples.tsx
+++ src/components/ClaimedExamples.tsx
@@ -1,8 +1,75 @@
+"use client";
+
 import Image from "next/image";
 import thumbImg from "../../public/300wd.jpg";
 import ytb from "../../public/youtube.svg";
+import { useEffect, useState } from "react";
+
+const VIDEO_URL =
+  "https://www.youtube.com/watch?v=4IenX7OHumk&pp=ygURd2lsZCBhbmltYWxzIGxpZmU%3D";
+
+interface VideoDetails {
+  title: string;
+  thumbnailUrl: string;
+}
 
 export default function ClaimedExamples() {
+  const [videoDetails, setVideoDetails] = useState<VideoDetails | null>(null);
+
+  useEffect(() => {
+    async function fetchVideoDetails() {
+      try {
+        const details = await getYoutubeVideoDetails(VIDEO_URL);
+        setVideoDetails(details);
+      } catch (error) {
+        console.error("Failed to fetch YouTube video details:", error);
+      }
+    }
+
+    fetchVideoDetails();
+  }, []);
+
+  async function getYoutubeVideoDetails(videoUrl: string): Promise<VideoDetails> {
+    const videoId = new URL(videoUrl).searchParams.get("v");
+    if (!videoId) {
+      throw new Error("Invalid YouTube video URL");
+    }
+
+    const apiKey = "AIzaSyBFQKXiPBiluxE3jtWnTvZH3A9K76A8afc"; // Substitua pela sua chave de API do YouTube
+    const apiUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${apiKey}`;
+
+    try {
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch YouTube video");
+      }
+
+      const data = await response.json();
+
+      if (!data.items || data.items.length === 0) {
+        throw new Error("Video not found");
+      }
+
+      const snippet = data.items[0]?.snippet;
+      if (!snippet) {
+        throw new Error("Snippet not found");
+      }
+
+      const videoTitle = snippet.title;
+      const thumbnailUrl = snippet.thumbnails?.medium?.url; // Escolha o tamanho da miniatura desejado
+
+      if (!videoTitle || !thumbnailUrl) {
+        throw new Error("Video details not found");
+      }
+
+      return { title: videoTitle, thumbnailUrl: thumbnailUrl };
+    } catch (error) {
+      throw new Error("Failed to fetch YouTube video details");
+    }
+  }
+
+  const videoUrl = videoDetails?.thumbnailUrl
   return (
     <div className="flex flex-col w-fit justify-center items-center">
       <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-3xl px-6 pb-4">
@@ -17,21 +84,24 @@ export default function ClaimedExamples() {
         height={25}
       />
       <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-2xl px-6 pb-4 bt-2">
-        World of the Wild | Episode 1: The Amazon Rainforest | Free Documentary
-        Nature
+        {videoDetails?.title}
       </p>
       <div className="relative text-center">
-        <Image
-          src={thumbImg}
-          alt="Youtube Video Thumbnail"
-          width={200}
-          height={200}
-          style={{ borderRadius: "5px", overflow: "hidden" }}
-        />
-        <div className="absolute right-4 bottom-2 text-white bg-gray-200/25 w-12">
-          <span className="text=white font-bold">12:01</span>
-        </div>
-      </div>
+  {videoUrl ? (
+    <Image
+      src={videoUrl}
+      alt="Youtube Video Thumbnail"
+      width={400}
+      height={300}
+      style={{ borderRadius: "5px", overflow: "hidden" }}
+    />
+  ) : (
+    <div>No video thumbnail available</div>
+  )}
+  <div className="absolute right-4 bottom-2 text-white bg-gray-800/75 w-12">
+    <span className="text=white font-bold">12:01</span>
+  </div>
+</div>
     </div>
   );
 }
